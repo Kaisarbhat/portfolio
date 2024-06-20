764a05def78ea1576e10f0d39cb96908617ce2a6
K4151R <kaisarahmadbhat1108@gmail.com>
2024-06-20 19:50:47 +0530

    Education added

---
+++ src/Components/Education.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Education() {
+  return (
+    <section>
+      <h2>Education</h2>
+      <ul>
+        <li>
+          <h3>Bachelor of Engineering, Kongu Engineering College, Erode, Tamil Nadu</h3>
+          <p>November 2021 — May 2025</p>
+          <p>CGPA 7.89 (upto 5th sem.)</p>
+        </li>
+        <li>
+          <h3>12th, Govt Boys Higher Secondary School, Baramulla, J&K</h3>
+          <p>Passed with 91.6%</p>
+        </li>
+        <li>
+          <h3>10th, S.R.M Welkin Educational Trust, Sopore, J&K</h3>
+          <p>Passed with 86%</p>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Education;
