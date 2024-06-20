87cbadd091f724928a154d276e0e33b18abe5275
K4151R <kaisarahmadbhat1108@gmail.com>
2024-06-20 19:50:17 +0530

    Skils added

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Skills() {
+  return (
+    <section>
+      <h2>Skills</h2>
+      <ul>
+        <li>Programming: C, Java, Python, DSA</li>
+        <li>Web Development: HTML, CSS, JavaScript, BootStrap, ReactJs, Tailwind CSS, SpringBoot</li>
+        <li>Database Management: MySQL, MongoDB</li>
+        <li>Other Tools and Techs: Canva, VSCode, Git and Github, Linux</li>
+        <li>Excellent communication and interpersonal skills</li>
+        <li>Strong organizational and multitasking skills</li>
+        <li>Time Management and Prioritization</li>
+        <li>Fast Learner</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Skills;
