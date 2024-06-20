e1f57e3a15f1633ceb14952ff70008e4812212f0
K4151R <kaisarahmadbhat1108@gmail.com>
2024-06-20 19:51:17 +0530

    Projects added

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <section>
+      <h2>Projects</h2>
+      <ul>
+        <li>
+          <h3>Applr - A Social Networking Site</h3>
+          <p>Developed a Social media Website using ReactJs, MaterialUI, TailWindCSS, MySQL and SpringBoot</p>
+        </li>
+        <li>
+          <h3>Bank Management System</h3>
+          <p>Developed Using Python and MySQL</p>
+        </li>
+        <li>
+          <h3>Realtime Face Mask Detector</h3>
+          <p>Developed a Deep Learning project for real time mask detection using Python, OpenCV, TensorFlow</p>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Projects;
