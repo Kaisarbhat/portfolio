095bbd77c2d8f8e89aba998922ab8459cb5fe4f4
K4151R <kaisarahmadbhat1108@gmail.com>
2024-06-20 19:49:01 +0530

    Profile added

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <section>
+      <h2>Profile</h2>
+      <p>As someone well-versed in Computer Science, I am eager to join a dynamic team and take on challenging responsibilities that allow me to utilize my skills and expertise. I have an unyielding passion for technology, coupled with strong analytical abilities and a constant pursuit of professional development. I am confident in my ability to make meaningful contributions towards the growth and success of your organization.</p>
+    </section>
+  );
+}
+
+export default Profile;
