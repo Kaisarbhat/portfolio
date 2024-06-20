a3cef94c91f5a1e31fcd7b084a8a2a8a560d5935
K4151R <kaisarahmadbhat1108@gmail.com>
2024-06-20 19:51:37 +0530

    Contact added

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>Address: House no.230A Taleem Nagar, Rohama, Baramulla, 193301</p>
+      <p>Phone: 8082508099</p>
+      <p>Email: kaisarahmadbhat1108@gmail.com</p>
+      <h3>Links</h3>
+      <ul>
+        <li>GitHub: <a href="https://github.com/Kaisarbhat" target="_blank" rel="noopener noreferrer">https://github.com/Kaisarbhat</a></li>
+        <li>LeetCode: <a href="https://leetcode.com/K4151R/" target="_blank" rel="noopener noreferrer">https://leetcode.com/K4151R/</a></li>
+      </ul>
+    </section>
+  );
+}
+
+export default Contact;
